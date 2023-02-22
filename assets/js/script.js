21c862e90ad63f55c7c613e38ea8353737af7fb5
thomasjshea <thomasjshea89@gmail.com>
2023-02-21 21:22:31 -0600

    added local storage and functions for time out and game win

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Declare element variables
 var startButtonEl = document.getElementById('start-button');
 var startScreenEl = document.querySelector('.start-screen');
 var questionContainerEl = document.getElementById('question-container');
@@ -6,9 +7,17 @@ var answerGridEl = document.getElementById('answer-buttons');
 var answerButtonEl = document.querySelector('.answer-button');
 var endScreenEl = document.querySelector('.end-screen');
 var timerEl = document.querySelector('.timer');
+var formEl= document.getElementById('form');
+var submitButtonEl = document.getElementById('submit-button')
+var scoreScreenEl = document.getElementById('score-screen')
+var initialsInput = document.getElementById('initials')
 
-var currentQuestionIndex
-var timeLeft
+// Create an empty array to store the high scores
+var highScores = []
+
+// Declare variables to be called later
+var currentQuestionIndex, timeLeft, timeInterval, correctAnswer, wrongAnswer
+
 // Adds event listener to start button to call function startQuiz
 startButtonEl.addEventListener('click', startQuiz);
 
@@ -17,52 +26,72 @@ function startQuiz() {
     startScreenEl.classList.add('hide')
     currentQuestionIndex = 0
     questionContainerEl.classList.remove('hide');
-    // Calls countdown function to start the timer
+    // Calls countdown function to start the timer when you click the start button
     countdown()
     // calls nextQuestion function to display the next Question
     nextQuestion()
 }
 
 function countdown() {
-    // start with 75 seconds
-    timeLeft = 75;
-
-    var timeInterval = setInterval(function() {
+    // start with 50 seconds
+    timeLeft = 50;
+    // Counts down in one second intervals
+    timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = "Time: " + timeLeft + " seconds";
             timeLeft--
+            // Changes text to read "second" when one second remaining
         } else if (timeLeft === 1) {
-            timerEl.textContent= "Time: " + timeLeft + " second"; 
+            timerEl.textContent = "Time: " + timeLeft + " second";
             timeLeft--
+            // When clock reaches zero stop the timer and display the end page
         } else {
+            timeLeft = 0
             timerEl.textContent = "Time: " + timeLeft;
             clearInterval(timeInterval)
             questionContainerEl.classList.add('hide');
             endScreenEl.classList.remove('hide');
+            timeOut()
         }
     }, 1000)
 }
+// displays a message if you run out of time
+function timeOut() {
+    var youLose = document.createElement('h1')
+    youLose.textContent = "You've run out of Time!"
+    endScreenEl.appendChild(youLose)
+}
 
 function nextQuestion() {
+    // resets the question and answer choices with each new question
     resetQuestion()
+    // shows the next question 
     showQuestion(questions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
+    // Sets the text for Question element to come from the question array
     questionEl.innerText = question.question
+    // Creates a button for each answer in the question array
     question.answers.forEach(answer => {
         var button = document.createElement('button')
+        // Sets the text for the button to be the text from the answers array
         button.innerText = answer.text
+        // Apply classes to the answer buttons
         button.classList.add('button')
         button.classList.add('answer-button')
+        // sets the data element for correct answers to correct
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
+        // Add event listener for each answer button
         button.addEventListener('click', selectAnswer)
+        // appends answer button to end of the answer grid
         answerGridEl.appendChild(button)
     })
 }
 
+// function to reset answer choices
 function resetQuestion() {
     while (answerGridEl.firstChild) {
         answerGridEl.removeChild(answerGridEl.firstChild)
@@ -70,32 +99,66 @@ function resetQuestion() {
 }
 
 function selectAnswer(event) {
+    // sets the clicked answer as the selection
     var selectedButton = event.target
     var correct = selectedButton.dataset.correct
+    // creates an array from possible answers, sets class 'correct' for correct answers
     Array.from(answerGridEl.children).forEach(button => {
         setStatus(button, button.dataset.correct)
     })
+    // if answer is correct, display correct and move to next question
+    if (correct) {
+        correctAnswer = document.createElement('div')
+        correctAnswer.textContent = "Correct!"
+        correctAnswer.classList.add('correct')
+        questionContainerEl.appendChild(correctAnswer)
+    // if answer is incorrect, display incorrect, subtract 10 seconds and move to next question
+    } else {
+        wrongAnswer = document.createElement('div')
+        wrongAnswer.textContent = "Wrong!"
+        wrongAnswer.classList.add('wrong')
+        questionContainerEl.appendChild(wrongAnswer)
+        timeLeft = timeLeft - 10
+    }
+    // if there are questions remaining move to next question
     if (questions.length > currentQuestionIndex + 1) {
         currentQuestionIndex++
         nextQuestion()
+    // if this is the last question, move to the end screen and trigger game win function
     } else {
         questionContainerEl.classList.add('hide')
         endScreenEl.classList.remove('hide')
-    }
+        gameWin();
+        clearInterval(timeInterval);
 
-    if (correct){
-        var correctAnswer = document.createElement('div')
-        correctAnswer.textContent = "Correct!"
-        correctAnswer.classList.add('result')
-        questionContainerEl.appendChild(correctAnswer)
-    } else {
-        var wrongAnswer = document.createElement('div')
-        wrongAnswer.textContent = "Wrong!"
-        wrongAnswer.classList.add('result')
-        questionContainerEl.appendChild(wrongAnswer)
-        timeLeft-10
     }
+}
 
+// creates a game over function allowing the user to enter initals and log score
+function gameWin() {
+    var youWin = document.createElement('h1');
+    var enterScore = document.createElement('h2');
+    endScreenEl.appendChild(youWin)
+    endScreenEl.appendChild(enterScore)
+    formEl.classList.remove('hide')
+    youWin.textContent = "Congratulations, your score is " + (timeLeft + 1)
+    enterScore.textContent = "Please enter your Initials"
+    submitButtonEl.addEventListener('click', showScores)
+}
+
+
+
+function showScores(event) {
+    event.preventDefault()
+    var score = {
+        Name: initialsInput.value,
+        Score: (timeLeft + 1)
+    }
+    highScores.push(score)
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    endScreenEl.classList.add('hide')
+    formEl.classList.add('hide')
+    scoreScreenEl.classList.remove('hide')
 }
 
 function setStatus(element, correct) {
